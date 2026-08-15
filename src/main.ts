f0f45b06b6cac700ca0302f474e62153c0874723
KhengPhet <khengphet237@gmail.com>
2026-08-15 19:55:24 +0700

    fix main core

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,81 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import {
+  NestFactory
+} from '@nestjs/core';
+
+import {
+  ValidationPipe
+} from '@nestjs/common';
+
+import {
+  ConfigService
+} from '@nestjs/config';
+
 import { AppModule } from './app.module';
-import { MulterExceptionFilter } from './common/filters/multer-exception.filter';
+
+import {
+  MulterExceptionFilter
+} from './common/filters/multer-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
 
-  const config = app.get(ConfigService);
+  const app =
+    await NestFactory.create(AppModule);
+
+  const config =
+    app.get(ConfigService);
+
+  // =========================
+  // VALIDATION
+  // =========================
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
       forbidNonWhitelisted: false,
-    }),
+    })
   );
 
-  app.useGlobalFilters(new MulterExceptionFilter());
+  // =========================
+  // FILTER
+  // =========================
+
+  app.useGlobalFilters(
+    new MulterExceptionFilter()
+  );
+
+  // =========================
+  // CORS
+  // =========================
+
+  const corsOrigin =
+    config.get<string>(
+      'CORS_ORIGIN',
+      'http://localhost:4200'
+    );
 
   app.enableCors({
-    origin: config.get<string>('CORS_ORIGIN', 'http://localhost:4200'),
+    origin: corsOrigin,
     credentials: true,
   });
 
-  const port = Number(config.get<string>('PORT', '8080'));
-  await app.listen(port ,"0.0.0.0");
-  console.log(`API running on http://localhost:${port}`);
+  // =========================
+  // PORT
+  // =========================
+
+  const port =
+    Number(
+      config.get<string>('PORT', '8080')
+    );
+
+  await app.listen(
+    port,
+    '0.0.0.0'
+  );
+
+  console.log(
+    `API running on port ${port}`
+  );
 }
+
 void bootstrap();
